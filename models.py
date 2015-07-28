c6e00b2ebe0bb2f1f5042c3375b0a8815cf5fa04
Moises Ribeiro <ribeirojr@gmail.com>
2015-07-28 21:45:48 +0200

    Models added

---
+++ models.py
@@ -0,0 +1,18 @@
+import os 
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Thanks(Base):
+	__tablename__ = 'Thanks'
+	id = Column(Integer, primary_key=True)
+	email = Column(String(250), nullable=False)
+	message = Column(String(4000), nullable=False)
+
+engine = create_engine('sqlite:///database.db')
+
+Base.metadata.create_all(engine)
